ab78f0d63fec90c08696af93d47dd6b5807bc09c
peterjgomez30 <111475324+peterjgomez30@users.noreply.github.com>
2022-11-24 15:33:53 +0800

    netflix clone

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Navbar from "./components/Navbar";
+import Home from "./Pages/Home";
+import { Route, Routes } from "react-router-dom";
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Request.js
@@ -0,0 +1,11 @@
+const key = "d50794841e545966bf90e9992a39b985";
+
+const requests = {
+  requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+  requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
+  requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=2`,
+  requestHorror: `https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&query=horror&page=1&include_adult=false`,
+  requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
+};
+
+export default requests;
